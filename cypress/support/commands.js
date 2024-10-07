3e9de7aabfa7bfa4bd854f274266546d79ef34b1
JFalat <joanna.falatowicz@gmail.com>
2024-10-07 09:45:04 +0200

    przygodyZgitemZmianaMetodyPoprawa3

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,20 @@
 // ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('submitForm',(firstName, lastName, street, city,state, zipCode, phoneNumber, ssn, username, password,repeatedPassword) =>{
+    cy.get('#customer\\.firstName').type(textField, {force:true});
+    cy.get('#customer\\.lastName').type(textField, {force:true});
+    cy.get('#customer\\.address\\.street').type(textField, {force:true});
+    cy.get('#customer\\.address\\.city').type(textField, {force:true});
+    cy.get('#customer\\.address\\.state').type(textField, {force:true});
+    cy.get('#customer\\.address\\.zipCode').type(textField, {force:true});
+    cy.get('#customer\\.phoneNumber').type(textField, {force:true});
+    cy.get('#customer\\.ssn').type(textField, {force:true});
+
+    // Wpisanie losowej nazwy użytkownika
+    cy.get('#customer\\.username').type(textField, {force:true});
+    cy.get('#customer\\.password').type(textField, {force:true});
+    cy.get('#repeatedPassword').type(textField, {force:true});
+});
+Cypress.Commands.add('submitCheckbox', (check) => {
+    cy.get('#soapEndpoint').select('REST');
+});
