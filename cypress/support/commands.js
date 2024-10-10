14191783ffc8f0390b6d1b6e30e9d68585c85753
JFalat <joanna.falatowicz@gmail.com>
2024-10-10 14:23:39 +0200

    przygodyZgitemZmianaMetodyPoprawa5

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,47 +1,63 @@
 // ***********************************************
-
-Cypress.Commands.add('submitForm',(firstName, lastName, street, city,state, zipCode, phoneNumber, ssn, username, password,repeatedPassword) =>{
-    cy.get('#customer\\.firstName').type(textField, {force:true});
-    cy.get('#customer\\.lastName').type(textField, {force:true});
-    cy.get('#customer\\.address\\.street').type(textField, {force:true});
-    cy.get('#customer\\.address\\.city').type(textField, {force:true});
-    cy.get('#customer\\.address\\.state').type(textField, {force:true});
-    cy.get('#customer\\.address\\.zipCode').type(textField, {force:true});
-    cy.get('#customer\\.phoneNumber').type(textField, {force:true});
-    cy.get('#customer\\.ssn').type(textField, {force:true});
-
-    // Wpisanie losowej nazwy użytkownika
-    cy.get('#customer\\.username').type(textField, {force:true});
-    cy.get('#customer\\.password').type(textField, {force:true});
-    cy.get('#repeatedPassword').type(textField, {force:true});
+Cypress.Commands.add('fillTextField', (fieldSelector, value) => {
+    cy.get(fieldSelector)
+        .clear()   // Wyczyść pole przed wpisaniem nowej wartości
+        .type(value);  // Wpisz nową wartość
 });
+Cypress.Commands.add('submitForm', (
+    firstName,
+    lastName,
+    street,
+    city,
+    state,
+    zipCode,
+    phoneNumber,
+    ssn,
+    username,
+    password,
+    repeatedPassword
+) => {
+    cy.fillTextField('#customer\\.firstName', firstName);
+    cy.fillTextField('#customer\\.lastName', lastName);
+    cy.fillTextField('#customer\\.address\\.street', street);
+    cy.fillTextField('#customer\\.address\\.city', city);
+    cy.fillTextField('#customer\\.address\\.state', state);
+    cy.fillTextField('#customer\\.address\\.zipCode', zipCode);
+    cy.fillTextField('#customer\\.phoneNumber', phoneNumber);
+    cy.fillTextField('#customer\\.ssn', ssn);
+    cy.fillTextField('#customer\\.username', username);
+    cy.fillTextField('#customer\\.password', password);
+    cy.fillTextField('#repeatedPassword', repeatedPassword);
+});
+
 Cypress.Commands.add('submitCheckbox', (check) => {
     cy.get('#soapEndpoint').select('REST');
 });
 
-/**
- * Uniwersalna metoda do wyboru wartości z dropdowna
- * @param {string} dropdownSelector - Selector dla dropdowna (select lub input)
- * @param {string} optionValue - Wartość opcji, którą chcesz wybrać (value w select)
- * @param {string} [optionText] - Tekst opcji, którą chcesz wybrać (jeśli element ma opcje jako tekst)
- */
-Cypress.Commands.add('selectFromDropdown', (dropdownSelector, optionValue, optionText) => {
+
+Cypress.Commands.add('selectFromDropdown', (dropdownSelector, optionTextOrValue, isValue = false) => {
     cy.get(dropdownSelector).then($dropdown => {
         const tagName = $dropdown.prop('tagName').toLowerCase();
 
         if (tagName === 'select') {
-            // Dla <select> elementów
-            cy.get(dropdownSelector).select(optionValue);
+            // Dla elementów <select>
+            if (isValue) {
+                // Wybór opcji na podstawie atrybutu 'value'
+                cy.get(dropdownSelector).select(optionTextOrValue);
+            } else {
+                // Wybór opcji na podstawie tekstu
+                cy.get(dropdownSelector).select(optionTextOrValue);
+            }
         } else {
             // Dla custom dropdownów
-            cy.get(dropdownSelector).click(); // Otwieranie dropdowna
+            cy.get(dropdownSelector).click(); // Otwórz dropdown
 
-            if (optionText) {
-                // Szukaj opcji po tekście
-                cy.contains(optionText).click();
+            if (isValue) {
+                // Szukaj opcji po 'value'
+                cy.get(`[value="${optionTextOrValue}"]`).click();
             } else {
-                // Szukaj opcji po value
-                cy.get(`[value="${optionValue}"]`).click();
+                // Szukaj opcji po tekście
+                cy.contains(optionTextOrValue).click();
             }
         }
     });
