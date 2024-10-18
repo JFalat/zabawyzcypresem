0c485e7401b60c3f660f90947977ed1450bfbf2d
JFalat <joanna.falatowicz@gmail.com>
2024-10-18 11:41:54 +0200

    przygodyZgitemNowaStrona2

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,17 @@
-// ***********************************************
-Cypress.Commands.add('loginToSauceDemo', (username = 'standard_user', password = 'secret_sauce') => {
-    // Wejście na stronę logowania
-    cy.visit('https://www.saucedemo.com/');
+Cypress.Commands.add('loginToOrangeHRM', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    cy.get('input[name="username"]').type('Admin');
+    cy.get('input[name="password"]').type('admin123');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/dashboard');
+    cy.contains('Dashboard').should('be.visible');
+});
+Cypress.Commands.add('visitPage', (url) => {
+    // Odwiedź przekazany URL
+    cy.visit(url);
 
-    // Wprowadzenie nazwy użytkownika
-    cy.get('#user-name').clear().type(username);
-
-    // Wprowadzenie hasła
-    cy.get('#password').clear().type(password);
-
-    // Kliknięcie przycisku "Login"
-    cy.get('#login-button').click();
+    // Zweryfikuj, czy URL jest poprawny po przejściu
+    cy.url().should('include', url.split('/').pop());
 });
 
 Cypress.Commands.add('clickButton', (buttonSelector) => {
@@ -25,4 +26,11 @@ Cypress.Commands.add('enterText', (inputSelector, text) => {
 
 Cypress.Commands.add('clickLink', (linkSelector) => {
     cy.get(linkSelector).click();
+});
+Cypress.Commands.add('selectFromDropdown', (dropdownSelector, optionText) => {
+    // Kliknij dropdown, aby go otworzyć
+    cy.get(dropdownSelector).click();
+
+    // Wybierz opcję z dropdowna na podstawie jej tekstu
+    cy.get('div[role="listbox"]').contains(optionText).click();
 });
